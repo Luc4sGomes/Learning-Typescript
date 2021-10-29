42b8897c2e7dcf222ec08182320145da08b51feb
lucas <lucasgominhaa448@gmail.com>
2021-10-29 12:11:33 -0300

    added changes in file union

--- src/A1-type-annotation/A11-type-union.ts
+++ src/A1-type-annotation/A11-type-union.ts
@@ -1,5 +1,9 @@
-function add(a: number | string, b: number | string) {
-  if (typeof a === 'number' && typeof b === 'number') return a + b;
+function addOrConcat(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+  return `${a}${b}`;
 }
 
-console.log(add(10, 30));
+console.log(addOrConcat(10, 20));
+console.log(addOrConcat('10', '20'));
