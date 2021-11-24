e61b09524ca3baf3140b37fbcfd69e4d32d47729
lucas <lucasgominhaa448@gmail.com>
2021-11-23 21:06:48 -0300

    added study about class in ts

---
+++ src/A3-orientacao-objetos/classes.ts
@@ -0,0 +1,32 @@
+export class Empresa {
+  //criando uma empresa
+  public readonly name: string;
+  private readonly colaboradores: Colaborador[] = []; //lista de colaboradores
+  protected readonly CNPJ: string;
+
+  constructor(name: string, CNPJ: string) {
+    this.name = name;
+    this.CNPJ = CNPJ;
+  }
+
+  adicionaColab(colab: Colaborador): void {
+    //adiciona um colaborador na lista de colaboradores
+    this.colaboradores.push(colab);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  //criando um colaborador
+  constructor(public readonly name: string, public readonly lastName: string) {}
+}
+
+const colab1 = new Colaborador('lucas', 'gomes');
+const colab2 = new Colaborador('lucas', 'gomes');
+const colab23 = new Colaborador('lucas', 'gomes');
+const colab24 = new Colaborador('lucas', 'gomes');
