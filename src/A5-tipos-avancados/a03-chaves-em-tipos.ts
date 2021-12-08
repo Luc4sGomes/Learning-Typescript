fdcfa4e892e249d5fe8f8e50e341898e343ab053
lucas <lucasgominhaa448@gmail.com>
2021-12-08 11:49:47 -0300

    added study about keys in types | typescript

---
+++ src/A5-tipos-avancados/a03-chaves-em-tipos.ts
@@ -0,0 +1,32 @@
+//como utilizar chaves de tipos, isso ajuda a manter o codigo mais limpo e evita que voce fique repetindo trechos do seu codigo que são iguais
+
+type Veiculo = {
+  marca: string;
+  ano: string;
+};
+
+type Car = {
+  brand: Veiculo['marca'];
+  year: Veiculo['ano'];
+  name: string;
+};
+
+const carro: Car = {
+  brand: 'ford',
+  year: '2020',
+  name: 'name',
+};
+
+interface Veic {
+  marca: string;
+  ano: string;
+}
+
+class carro1 implements Veic {
+  marca: string;
+  ano: string;
+  constructor(marca: string, ano: string) {
+    this.marca = marca;
+    this.ano = ano;
+  }
+}
