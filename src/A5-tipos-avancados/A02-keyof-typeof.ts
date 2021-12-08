c642873b588628a7fa1dd1de3477138d1c991218
lucas <lucasgominhaa448@gmail.com>
2021-12-08 11:35:07 -0300

    added study about keyof and type of in typescript

---
+++ src/A5-tipos-avancados/A02-keyof-typeof.ts
@@ -0,0 +1,18 @@
+type CoresObj = typeof coresObj;
+type CoresChaves = keyof CoresObj;
+
+const coresObj = {
+  vermelho: 'red',
+  verde: 'green',
+  azul: 'blue',
+  roxo: 'purple',
+};
+
+function traduzirCor(
+  cor: 'vermelho' | 'verde' | 'azul',
+  cores: typeof coresObj,
+) {
+  return cores[cor];
+}
+
+console.log(traduzirCor('vermelho', coresObj));
