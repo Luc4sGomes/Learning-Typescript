a139e460fd72675ff39e0db7452523ab6bb75d81
lucas <lucasgominhaa448@gmail.com>
2022-01-02 18:23:38 -0300

    added study about getters and setters

--- src/documentation-ts/class-documentation.ts
+++ src/documentation-ts/class-documentation.ts
@@ -73,12 +73,114 @@ g.name = 'also is not ok'; //ainda nao funciona por causa da propriedade que é 
 //os construtores de classes sao muito semelhantes as funcoes. voce pode adicionar parametros com anotacoes de tipo, valores padrao e sobrecargas
 
 class Point3 {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 
-    //assinatura normal por padrao
-    constructor(x = 0; y = 0) {
-        this.x = x;
-        this.y = y;
+  //assinatura normal por padrao
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Point4 {
+  //sobrecargas
+  constructor(x: number, y: string);
+  constructor(s: string);
+  constructor(xs: any, y?: any) {
+    //tbd
+  }
+}
+
+//existem apenas algumas diferenças entre as assinaturas do construtor de classe e as assinaturas de funcao:
+
+//os contrutores nao podem ter parametros de tipo, eles pertencem a declaração da classe externa sobre a qual aprenderemos mais tarde.
+
+//construtores nao podem ter anotações de tipos de retorno - o tipo de instancia de classe é sempre o que é retornado
+
+//=====================================================================//
+
+//SuperChamadas
+//Assim como no javascript se voce tiver uma classe base, precisara chamar super(); o corpo do construtor antes de usar qualquer this.membro;
+class Base {
+  k = 4;
+}
+
+class Derived extends Base {
+  constructor() {
+    super(); //o super deve ser chamado antes de acessar o "this" no construtor derivado da classe
+    //imprime um valor errado em ES5 lança um erro de execao no ES6
+    console.log(this.k);
+  }
+}
+//esquecer de chamar o super é um erro facil de cometer em javascript, mas o Typescript dirá para voce quando for necessario.
+
+//=======================================================================//
+
+//Metodos
+//Uma propriedade de funcao em uma classe é chamada de metodo. os metodos podem usar todas as anotações do mesmo tipo como funcoes e construtores:
+
+class Point5 {
+  x = 10;
+  y = 11;
+
+  scale(n: number): void {
+    this.x *= n;
+    this.y *= n;
+  }
+}
+//alem das anotacoes de tipo padrao, o TypeScript nao adiciona nada de novo aos metodos
+//observe que dentro do corpo de um metodo, ainda é obrigatorio acessar os campos e outros metodos via "this" um nome nao qualificado em um corpo de metodo sempre se referira a algo no escopo global nao ao escopo da classe!
+
+let x = 0;
+
+class C {
+  x = 'hello';
+
+  m() {
+    //isso esta tentando modificar "x" da linha 135 e nao a propriedade da classes
+    x = 'world';
+    //tipo "string" nao é assinavel para o tipo "number"
+  }
+}
+
+//============================================================//
+//Getter and Setters
+//as classes tambem podem ter acessores
+
+class C2 {
+  _length = 0;
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+}
+//O Typescript tem algumas regras de inferencia especias para acessadores:
+//1: se "get" existe, mas nao "set", a propriedade é automaticamente readOnly
+//2: se o tipo do parametro setter nao for especificado, ele é inferido a partir do tipo do retorno do "getter"
+//3: getters and setters devem ter a mesma visibilidade de membros
+
+//desde Typescript 4.3 é possivel ter acessores com diferentes tipos de obtencao e configuração:
+
+class Thing {
+  _size: 0;
+
+  get size(): number {
+    return this._size;
+  }
+
+  set size(value: string | number | boolean) {
+    const num = Number(value);
+
+    //nao permitido NaN, Infinity etc...
+
+    if (!Number.isFinite(num)) {
+      this._size = 0;
+      return;
     }
+    this._size = num;
+  }
 }
