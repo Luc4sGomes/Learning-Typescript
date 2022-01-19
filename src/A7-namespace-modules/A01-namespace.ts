d1ad82841136028e85307101b94d4d001021057a
lucas <lucasgominhaa448@gmail.com>
2022-01-19 14:24:36 -0300

    added module with study about namespaces and modules

---
+++ src/A7-namespace-modules/A01-namespace.ts
@@ -0,0 +1,22 @@
+//namespace criar qualquer coisa dentro de um escopo
+
+/*eslint-disable @typescript-eslint/no-namespace*/
+namespace MeuNameSpace {
+  export const nameOfNamespace = 'lucas';
+
+  export class PersonOfNameSpace {
+    constructor(public name: string) {}
+  }
+
+  const PersonOfNameSpace = new PersonOfNamespace('lucas');
+  console.log(PersonOfNameSpace);
+
+  export namespace OutroNamespace {
+    export const nameOfNamespace = 'nome de outro namespace';
+  }
+}
+
+const pessoa = new MeuNameSpace.PersonOfNameSpace('Lucas');
+console.log(pessoa);
+
+export default 1;
