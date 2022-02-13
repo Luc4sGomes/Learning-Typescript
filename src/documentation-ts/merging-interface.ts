9ecad0e43acd227dc7a9b479e9af069e35624125
lucas <lucasgominhaa448@gmail.com>
2022-02-12 23:28:05 -0300

    added study about mergining interfaces

---
+++ src/documentation-ts/merging-interface.ts
@@ -0,0 +1,16 @@
+//mergining interfaces
+
+import { Cat } from '../A3-orientacao-objetos/PilaresPOO';
+
+interface Box {
+  height: number;
+  width: number;
+}
+
+interface Box {
+  scale: number;
+}
+
+const box: Box = { height: 5, width: 6, scale: 10 };
+
+//the way more simple to explain how is the process of merging interfaces
