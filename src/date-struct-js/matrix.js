00c87efb9c611214d8891a6ae0547bcfcb9de488
lucas <lucasgominhaa448@gmail.com>
2022-08-15 22:44:56 -0300

    added final study about matrix

---
+++ src/date-struct-js/matrix.js
@@ -0,0 +1,13 @@
+//Matrix or matriz is a multidimensional vector
+//that means is an Array in another Array.
+//we can have many levels
+
+const students = [
+  ['Lucas', 21, 'PB'],
+  ['Eduarda', 17, 'SP'],
+  ['Luiz', 72, 'RJ'],
+];
+
+console.log(students[0][1]);
+console.log(students[1][1]);
+console.log(students[2][0]);
