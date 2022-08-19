f3261498770b4546f2a2015d8819616343320105
lucas <lucasgominhaa448@gmail.com>
2022-08-19 11:15:38 -0300

    added study about functional programming initial

---
+++ src/functional-programming/func.js
@@ -0,0 +1,15 @@
+//funcao que eleva o numero ao quadrado
+
+//Imperativo: faça da seguinte forma
+
+let number = 2;
+
+function square() {
+  return number * number;
+}
+
+number = square();
+
+//declarativo: o que fazer e nao como fazer
+
+const square2 = (n) => n * n;
