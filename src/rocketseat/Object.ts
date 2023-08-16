93502df4f999a6a69ea77eaf15d5230bb8855c5e
lucas <lucasgominhaa448@gmail.com>
2023-08-16 17:35:36 -0300

    :heavy_check_mark added study about types in typescript with objects

---
+++ src/rocketseat/Object.ts
@@ -0,0 +1,17 @@
+type Point2 = {
+    x: number;
+    y: number;
+}
+
+type Usuario = {
+    name: string;
+    idade: string;
+    age: number;
+    isAdmin: boolean;
+}
+
+function printCordenada(pontos: Point2) {
+    console.log(`o eixo X é: ${pontos.x} e o eixo y é ${pontos.y}`)
+}
+
+printCordenada({x: 101, y: 50});
